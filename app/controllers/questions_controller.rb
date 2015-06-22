4a039abe493bd4d2e05d5f6915c89826189412b7
Joseph Tessel <Tessel.Joseph@gmail.com>
2015-06-22 17:52:29 -0400

    Hyperlinked the questions, but don't linkk anywhere

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
     @questions = []
     lists = 0
     while lists < Question.count
-    @questions << [Question.all[lists].title, Question.all[lists].created_at]
+    @questions << [Question.all[lists].id, Question.all[lists].title, Question.all[lists].created_at, Question.all[lists].updated_at, Question.all[lists].body]
     lists+=1
 
     end
--- db/migrate/20150622203539_add_question_body.rb
+++ db/migrate/20150622203539_add_question_body.rb
@@ -1,4 +1,5 @@
 class AddQuestionBody < ActiveRecord::Migration
   def change
+    add_column :questions, :body, :text
   end
 end
