40f4b4e5126902e3d30ab8865e4f40d8dbcdd0c5
Joseph Tessel <Tessel.Joseph@gmail.com>
2015-06-23 18:33:53 -0400

    add the ability to ask questions, add parameters corrosponding to those questions, store onto database

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,9 @@
 class QuestionsController < ApplicationController
   def new
+@entry = Question.new
   end
   def index
+    @entry = Question.new
     @questions = []
     lists = 0
     while lists < Question.count
@@ -13,8 +15,33 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @title = Question.all.where(id: params[:id].first)
-    
+    @title = Question.all.where(id: params[:id])[0]
+
   end
+
+  def create
+    @questions = Question.new(questions_params)
+     if @questions.save
+       flash[:notice] = "Question created"
+       redirect_to :back
+     else
+       flash[:notice] = "Question not added"
+      redirect_to :back
+     end
+  end
+
+  def destroy
+    @questions = Question.new(questions_params)
+  end
+
+  private
+
+def questions_params
+  params.require(:question).permit(
+    :title,
+    :body
+  )
+end
+
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,11 @@
 class Question < ActiveRecord::Base
 
-  validates :title,
+  validates :title, length: { minimum: 40, too_short: "%{count} characters is the minimum allowed"  },
     presence: true,
     uniqueness: true
+
+    validates :body,
+      length: { minimum: 150, too_short: "%{count} characters is the minimum allowed" },
+      presence: true,
+      uniqueness: true
 end
